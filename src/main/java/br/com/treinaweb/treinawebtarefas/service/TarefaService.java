60c7d545d42a8dd59714729fd79419bfbda7f511
Cleyson Lima <cleysonph@gmail.com>
2021-01-24 02:56:11 -0300

    criação de tarefa a partir de uma api

--- src/main/java/br/com/treinaweb/treinawebtarefas/service/TarefaService.java
+++ src/main/java/br/com/treinaweb/treinawebtarefas/service/TarefaService.java
@@ -3,12 +3,15 @@ package br.com.treinaweb.treinawebtarefas.service;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpDelete;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.util.EntityUtils;
@@ -78,7 +81,24 @@ public class TarefaService {
     }
 
     public void cadastrarTarefa(Tarefa tarefa) {
-        repository.save(tarefa);
+        String url = webService + "tarefas";
+
+        try {
+            CloseableHttpClient client = HttpClientBuilder.create().build();
+            HttpPost httpPost = new HttpPost(url);
+
+            ObjectMapper mapper = new ObjectMapper();
+            mapper.setSerializationInclusion(Include.NON_EMPTY);
+            String tarefaJson = mapper.writeValueAsString(tarefa);
+
+            httpPost.setEntity(new StringEntity(tarefaJson));
+            httpPost.setHeader("Accept", "application/json");
+            httpPost.setHeader("Content-Type", "application/json");
+
+            client.execute(httpPost);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 
     public void editarTarefa(Tarefa tarefa) {
